8fbce2547f263dfac1deba3356e91e5e5eed1f68
BillyMaysHere32 <kgetz32@gmail.com>
2023-01-26 08:47:06 -0800

    added scrollable button

--- src/App.js
+++ src/App.js
@@ -15,12 +15,16 @@ function App() {
       <img src={require('./assests/background.jpg')} className="background" alt="mountain" />
       <img src={require('./assests/Trees.png')} className="foreground" alt="trees"/>
       <h2 className="title display-1 fw-bold">Hello, I'm Kevin!</h2>
-      <div className="scroll-down"></div>
+      {/* <div className="scroll-down"></div> */}
+      <a class="scroll-down" href="#scrollspyHeading1"></a>
     </div>
     <div className="section">
       <Container>
         <Row className="d-flex flex-column mt-5">
-          <Col><About /></Col>
+          <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+            <h4 id="scrollspyHeading1"></h4>
+            <Col><About /></Col>
+          </div>
           <br></br>
           <Col><Projects /></Col>
           <Col><Contact /></Col>
